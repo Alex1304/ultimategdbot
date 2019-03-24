f36aad6f7dd55dd967a104026e5e2a7779a7058e
Alex1304 <mirandaa1304@gmail.com>
2019-03-24 16:08:49 +0100

    Guild join/leave are now logged

--- ultimategdbot-core/src/main/java/com/github/alex1304/ultimategdbot/core/nativeplugin/NativePlugin.java
+++ ultimategdbot-core/src/main/java/com/github/alex1304/ultimategdbot/core/nativeplugin/NativePlugin.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -14,13 +16,19 @@ import com.github.alex1304.ultimategdbot.api.Command;
 import com.github.alex1304.ultimategdbot.api.Plugin;
 import com.github.alex1304.ultimategdbot.api.database.GuildSettingsEntry;
 import com.github.alex1304.ultimategdbot.api.database.NativeGuildSettings;
+import com.github.alex1304.ultimategdbot.api.utils.BotUtils;
 import com.github.alex1304.ultimategdbot.api.utils.GuildSettingsValueConverter;
 import com.github.alex1304.ultimategdbot.api.utils.PropertyParser;
+
+import discord4j.core.event.domain.guild.GuildCreateEvent;
+import discord4j.core.event.domain.guild.GuildDeleteEvent;
+import discord4j.core.object.util.Snowflake;
 
 public class NativePlugin implements Plugin {
 	
 	private Bot bot;
 	private String aboutText;
+	private final Set<Snowflake> unavailableGuildIds = Collections.synchronizedSet(new HashSet<>());
 
 	@Override
 	public void setup(Bot bot, PropertyParser parser) {
@@ -34,7 +42,34 @@ public class NativePlugin implements Plugin {
 
 	@Override
 	public void onBotReady() {
-		return;
+		// Guild join
+		bot.getDiscordClients().flatMap(client -> client.getEventDispatcher().on(GuildCreateEvent.class))
+				.filter(event -> {
+					var id = event.getGuild().getId();
+					if (unavailableGuildIds.contains(id)) {
+						unavailableGuildIds.remove(id);
+						return false;
+					}
+					return true;
+				})
+				.map(GuildCreateEvent::getGuild)
+				.flatMap(guild -> bot.log(":inbox_tray: New guild joined: " + BotUtils.escapeMarkdown(guild.getName())
+						+ " (" + guild.getId().asString() + ")"))
+				.subscribe();
+		// Guild leave
+		bot.getDiscordClients().flatMap(client -> client.getEventDispatcher().on(GuildDeleteEvent.class))
+				.filter(event -> {
+					if (event.isUnavailable()) {
+						unavailableGuildIds.add(event.getGuildId());
+						return false;
+					}
+					unavailableGuildIds.remove(event.getGuildId());
+					return true;
+				})
+				.map(event -> event.getGuild().map(guild -> BotUtils.escapeMarkdown(guild.getName())
+						+ " (" + guild.getId().asString() + ")").orElse(event.getGuildId().asString() + " (no data)"))
+				.flatMap(str -> bot.log(":outbox_tray: Guild left: " + str))
+		.subscribe();
 	}
 	
 	@Override
