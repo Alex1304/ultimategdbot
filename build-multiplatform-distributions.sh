c6f741f7188668a608cbde2191aab3f5687af5f4
Alex1304 <mirandaa1304@gmail.com>
2021-06-09 21:30:28 +0200

    Fix build script

--- build-multiplatform-distributions.sh
+++ build-multiplatform-distributions.sh
@@ -2,26 +2,41 @@
 
 # Build for Linux
 
-if [ "$(mvn clean package)" ]; then
+mvn clean package
+
+if [ $? -ne 0 ]; then
+	echo "linux build failed"
 	exit
 fi
 
+echo "linux build successful"
+
 mkdir -p delivery/target/distribution
-file="$(find delivery/target/ -name "*.zip" -printf "%f\n")"
+file=`find delivery/target/ -name "*.zip" -printf "%f\n"`
 mv "delivery/target/$file" "delivery/target/distribution/${file%.*}_linux_x64.zip"
 
 # Build for MacOS
 
-if [ "$(mvn package -Djlink.jdk="$MACOS_JDK")" ]; then
+mvn package -Djlink.jdk="$MACOS_JDK"
+
+if [ $? -ne 0 ]; then
+	echo "macos build failed"
 	exit
 fi
+
+echo "macos build successful"
 
 mv "delivery/target/$file" "delivery/target/distribution/${file%.*}_macOS_x64.zip"
 
 # Build for Windows
 
-if [ "$(mvn package -Djlink.jdk="$WINDOWS_JDK")" ]; then
+mvn package -Djlink.jdk="$WINDOWS_JDK"
+
+if [ $? -ne 0 ]; then
+	echo "windows build failed"
 	exit
 fi
+
+echo "windows build successful"
 
 mv "delivery/target/$file" "delivery/target/distribution/${file%.*}_windows_x64.zip"
