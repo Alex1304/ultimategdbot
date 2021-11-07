dd58ae7c759b901c00e1bb271f166cfdfddbf3f3
Alex1304 <mirandaa1304@gmail.com>
2021-11-07 16:40:07 +0100

    Release version v7.0.0

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Version number required"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Next version number required"
+    exit 1
+fi
+
+set -e
+
+. ~/.bash_aliases
+mvn versions:set -DnewVersion=$1
+gacp "Release version v$1"
+git tag "v$1"
+git push --tags
+./build-multiplatform-distributions.sh
+scp "delivery/target/distribution/ultimategdbot-$1_linux_x64.zip" alex@alex1304.com:/home/alex/ultimategdbot/deploy
+mvn versions:set -DnewVersion=$2
+gacp "Prepare for next development iteration"
